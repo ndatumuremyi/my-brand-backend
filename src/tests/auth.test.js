949f2ba526ac827460f03b49a7f83e052a850736
ndatumuremyi <paternenught@gmail.com>
2023-01-10 09:43:30 +0200

    adding

--- src/tests/auth.test.js
+++ src/tests/auth.test.js
@@ -1,5 +1,7 @@
 import request from 'supertest'
 import {Requests} from "./requests.js";
+import {v4 as getUniqueId } from 'uuid';
+import User from "../models/User.js";
 
 const baseURL = "http://localhost:5000"
 describe("post /users/login", () => {
@@ -28,3 +30,27 @@ describe("patch /users/logout", () => {
     });
 
 })
+
+describe("POST user/signup", () => {
+    describe("Incorrect email or password", () => {
+        it(" 403 status code", async () => {
+            const response = await request(baseURL)
+                .post("/api/v1/users/signup")
+                .send({ email: "ndatumuremyi@gmail.com", password: "password" });
+            expect(response.statusCode).toBe(403);
+            // expect(response.body).not.toHaveProperty("token");
+            expect(response.body).toHaveProperty("error");
+        });
+    });
+    describe("New email and a valid password", () => {
+        it("201 status code, and user in db", async () => {
+            const email = `test${getUniqueId()}@gmail.com`;
+            const response = await request(baseURL)
+                .post("/api/v1/users/signup")
+                .send({ email: email, password: "password" });
+            const user = User.findOne({ email });
+            expect(response.statusCode).toBe(201);
+            expect(user).toBeDefined();
+        });
+    });
+});
